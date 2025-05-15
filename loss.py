6c5f85f00c1aebfcc83b59a68affe7cd17475ca9
wyfnice <157370701+wyfnice@users.noreply.github.com>
2025-05-15 18:54:16 +0800

    Update loss.py

--- loss.py
+++ loss.py
@@ -19,11 +19,11 @@ class SupConLoss(nn.Module):
     def __init__(self, temperature=0.07, contrast_mode='all',
                  base_temperature=0.07):
         super(SupConLoss, self).__init__()
-        self.temperature = temperature#温度参数
-        self.contrast_mode = contrast_mode#对比模式
-        self.base_temperature = base_temperature#基础温度参数
+        self.temperature = temperature
+        self.contrast_mode = contrast_mode
+        self.base_temperature = base_temperature
 
-    def forward(self, features, labels=None, mask=None):  # 向前传播方法，用于计算损失 mask对比掩码，形状为[bsz, bsz]，mask_{i,j}=1表示样本i和样本j属于同一类别
+    def forward(self, features, labels=None, mask=None): 
         """Compute loss for model. If both `labels` and `mask` are None,
         it degenerates to SimCLR unsupervised loss:
         https://arxiv.org/pdf/2002.05709.pdf
@@ -39,18 +39,18 @@ class SupConLoss(nn.Module):
                   if features.is_cuda
                   else torch.device('cpu'))
 
-        if len(features.shape) < 3: #检查输入特征的维度
+        if len(features.shape) < 3: 
             raise ValueError('`features` needs to be [bsz, n_views, ...],'
                              'at least 3 dimensions are required')
         if len(features.shape) > 3:
             features = features.view(features.shape[0], features.shape[1], -1)
 
-            # 归一化处理特征
+            
         features = F.normalize(features, p=2, dim=2)
 
 
         batch_size = features.shape[0]
-        if labels is not None and mask is not None:  # 根据输入的标签和掩码生成对比的掩码
+        if labels is not None and mask is not None: 
             raise ValueError('Cannot define both `labels` and `mask`')
         elif labels is None and mask is None:
             mask = torch.eye(batch_size, dtype=torch.float32).to(device)
@@ -63,7 +63,7 @@ class SupConLoss(nn.Module):
             mask = mask.float().to(device)
 
         contrast_count = features.shape[1]  #   2
-        contrast_feature = torch.cat(torch.unbind(features, dim=1), dim=0)#对比特征，将多个视图的特征串联起来，以便进行对比学习
+        contrast_feature = torch.cat(torch.unbind(features, dim=1), dim=0)
         #   256 x 512
         if self.contrast_mode == 'one':
             anchor_feature = features[:, 0]
@@ -74,8 +74,8 @@ class SupConLoss(nn.Module):
         else:
             raise ValueError('Unknown mode: {}'.format(self.contrast_mode))
 
-        # compute logits 计算锚点特征和对比特征之间的点乘 exp(zi·zj/t）
-        anchor_dot_contrast = torch.div( #点乘计算，通过锚点特征和对比特征之间的点乘计算得分，作为对比损失的一部分
+       
+        anchor_dot_contrast = torch.div
             torch.matmul(anchor_feature, contrast_feature.T),
             self.temperature)
         # for numerical stability
@@ -84,7 +84,7 @@ class SupConLoss(nn.Module):
         logits_max, _ = torch.max(anchor_dot_contrast, dim=1, keepdim=True)
         logits = anchor_dot_contrast - logits_max.detach()
 
-        # tile mask 掩码，使用标签或掩码来控制哪些样本需要考虑进行对比损失的计算
+       
         mask = mask.repeat(anchor_count, contrast_count)
 
         # mask-out self-contrast cases
@@ -97,13 +97,12 @@ class SupConLoss(nn.Module):
         mask = mask * logits_mask
 
         # compute log_prob
-        exp_logits = torch.exp(logits) * logits_mask # logits是由锚点特征计算得到的对比分数
+        exp_logits = torch.exp(logits) * logits_mask 
         log_prob = logits - torch.log(exp_logits.sum(1, keepdim=True))
 
-        # compute mean of log-likelihood over positive
-        mean_log_prob_pos = (mask * log_prob).sum(1) / mask.sum(1)  # 计算了正样本的对数概率的均值
-        loss = - (self.temperature / self.base_temperature) * mean_log_prob_pos  # 计算损失值 损失值是平均对数概率的加权和，权重由t和base_t决定
-        loss = loss.view(anchor_count, batch_size).mean()  # 对每个锚点的损失值求均值，得到最终的损失值
+       
+        loss = - (self.temperature / self.base_temperature) 
+        loss = loss.view(anchor_count, batch_size).mean()  
         return loss
 
 
