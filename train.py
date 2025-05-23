1ba13897d61b930bf0d48630151cba308ecb5f91
wyfnice <157370701+wyfnice@users.noreply.github.com>
2025-05-23 13:47:49 +0800

    Update train.py

--- train.py
+++ train.py
@@ -2,51 +2,38 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import torchvision
 import torchvision.transforms as transforms
 import argparse
 import os
 import torch.nn.functional as F
 import numpy as np
-from PIL import Image
-import PIL
-from torchvision import datasets, transforms
+from torchvision import datasets
 from loss import *
 from densenet import *
-import matplotlib.pyplot as plt
 from cutout import *
-from densenet import GradCAM
+from MedVitV2 import *
 from sklearn.model_selection import KFold
 from torch.utils.data import SubsetRandomSampler
 
-
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-
+os.environ["CUDA_VISIBLE_DEVICES"] = "6"
 
 def CrossEntropy(outputs, targets):
     log_softmax_outputs = F.log_softmax(outputs / 3, dim=1)
     softmax_targets = F.softmax(targets / 3, dim=1)
     return -(log_softmax_outputs * softmax_targets).sum(dim=1).mean()
 
-
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-parser = argparse.ArgumentParser(description='PyTorch TelluOdata Training with K-Fold Cross Validation')
+parser = argparse.ArgumentParser(description='K-Fold Training with Validation Only')
 parser.add_argument('--epoch', default=100, type=int)
-parser.add_argument('--alpha', default=50, type=float)
-parser.add_argument('--beta', default=0, type=float)
-parser.add_argument('--gama', default=0.8, type=float)
 parser.add_argument('--c_loss_weight', default=0.8, type=float)
 parser.add_argument('--model', default="densenet121", type=str)
-parser.add_argument('--supervision', default=True, type=bool)
-parser.add_argument('--n_splits', default=5, type=int, help='Number of folds for cross validation')
-parser.add_argument('--results_dir', default="Colondensent", type=str, help='Custom directory name for saving results')
+parser.add_argument('--n_splits', default=5, type=int)
+parser.add_argument('--results_dir', default="Colon_DenseNet", type=str)
 args = parser.parse_args()
 
 BATCH_SIZE = 128
 LR = 0.01
-epoch = 100
 size = (80, 80)
-
 
 transform1 = transforms.Compose([
     transforms.Resize(size),
@@ -73,53 +60,40 @@ transform_test = transforms.Compose([
     transforms.Resize(size)
 ])
 
+class TwoCropTransform:
+    def __init__(self, transform1, transform2):
+        self.transform1 = transform1
+        self.transform2 = transform2
+    def __call__(self, x):
+        return [self.transform1(x), self.transform2(x)]
 
-train_dir = os.path.join("/home/weiyafang/NewCL/CIFAR/NewColonDataset/trainval/")
-test_dir = os.path.join("/home/weiyafang/NewCL/CIFAR/NewColonDataset/test/")
-
-
+train_dir = "/home/weiyafang/DQDETR/CIFAR/NewColonDataset/trainval1/"
 train_dataset = datasets.ImageFolder(
     root=train_dir,
     transform=TwoCropTransform(transform1, transform2)
 )
-test_dataset = datasets.ImageFolder(
-    root=test_dir,
-    transform=transform_test
-)
-
 
 kfold = KFold(n_splits=args.n_splits, shuffle=True, random_state=42)
-
-
 fold_results = []
-
+all_val_labels_all = []
+all_val_preds_all = []
+all_val_probs_all = []
 
 def train_fold(fold_idx, train_idx, val_idx):
     print(f'Training Fold {fold_idx + 1}/{args.n_splits}')
-
 
     train_sampler = SubsetRandomSampler(train_idx)
     val_sampler = SubsetRandomSampler(val_idx)
 
     train_loader = torch.utils.data.DataLoader(
-        train_dataset,
-        batch_size=BATCH_SIZE,
-        sampler=train_sampler,
-        num_workers=4
-    )
-
+        train_dataset, batch_size=BATCH_SIZE, sampler=train_sampler, num_workers=4)
     val_loader = torch.utils.data.DataLoader(
-        train_dataset,
-        batch_size=BATCH_SIZE,
-        sampler=val_sampler,
-        num_workers=4
-    )
+        train_dataset, batch_size=BATCH_SIZE, sampler=val_sampler, num_workers=4)
 
-
-    if args.model == "densenet121":
+    if args.model == "medvit":
+        model_name = MedViT_base
+    elif args.model == "densenet121":
         model_name = densenet121
-    elif args.model == "densenet169":
-        model_name = densenet169
     elif args.model == "densenet201":
         model_name = densenet201
     elif args.model == "densenet161":
@@ -135,13 +109,11 @@ def train_fold(fold_idx, train_idx, val_idx):
     best_acc = 0
     best_model_state = None
 
-  
     for epoch in range(args.epoch):
         if epoch in [30, 60, 90]:
             for param_group in optimizer.param_groups:
                 param_group['lr'] /= 10
 
-        
         net.train()
         sum_loss = 0.0
         correct = 0.0
@@ -167,7 +139,6 @@ def train_fold(fold_idx, train_idx, val_idx):
                     c_loss += criterion_c(features, labels)
 
             loss = loss + args.c_loss_weight * c_loss
-
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
@@ -177,96 +148,53 @@ def train_fold(fold_idx, train_idx, val_idx):
             total += float(labels.size(0))
             correct += float(predicted.eq(labels.data).cpu().sum())
 
-            if i % 50 == 0:
-                print('[Fold:%d, epoch:%d, iter:%d] Loss: %.03f contractive loss:%.03f| Acc: %.4f%%'
-                      % (fold_idx + 1, epoch + 1, (i + 1 + epoch * length),
-                         sum_loss / (i + 1), c_loss / (i + 1), 100 * correct / total))
-
-      
+        # 验证阶段
         net.eval()
         val_correct = 0.0
         val_total = 0.0
-        val_loss = 0.0
+        all_val_labels = []
+        all_val_preds = []
+        all_val_probs = []
 
         with torch.no_grad():
             for data in val_loader:
                 images, labels = data
                 if isinstance(images, list):
-                    images = images[0]  
+                    images = images[0]
                 images, labels = images.to(device), labels.to(device)
                 outputs, _ = net(images)
-                loss = criterion(outputs, labels)
-                val_loss += loss.item() * images.size(0)
+                probs = torch.softmax(outputs, dim=1)
                 _, predicted = torch.max(outputs.data, 1)
+
                 val_total += labels.size(0)
                 val_correct += (predicted == labels).sum().item()
 
-        val_acc = 100 * val_correct / val_total
-        val_loss /= val_total
-        print('Fold %d Validation Accuracy: %.4f%%, Validation Loss: %.4f' %
-              (fold_idx + 1, val_acc, val_loss))
+                all_val_labels.extend(labels.cpu().numpy())
+                all_val_preds.extend(predicted.cpu().numpy())
+                all_val_probs.extend(probs.cpu().numpy())
 
-     
+        val_acc = 100 * val_correct / val_total
+        print('Fold %d Epoch %d - Validation Accuracy: %.4f%%' % (fold_idx + 1, epoch + 1, val_acc))
+
         if val_acc > best_acc:
             best_acc = val_acc
             best_model_state = net.state_dict()
+            best_val_labels = np.array(all_val_labels)
+            best_val_preds = np.array(all_val_preds)
+            best_val_probs = np.array(all_val_probs)
 
- 
     torch.save(best_model_state, f"{args.model}_fold_{fold_idx + 1}_best.pth")
+    np.save(os.path.join(args.results_dir, f"fold_{fold_idx + 1}_val_labels.npy"), best_val_labels)
+    np.save(os.path.join(args.results_dir, f"fold_{fold_idx + 1}_val_preds.npy"), best_val_preds)
+    np.save(os.path.join(args.results_dir, f"fold_{fold_idx + 1}_val_probs.npy"), best_val_probs)
 
+    all_val_labels_all.append(best_val_labels)
+    all_val_preds_all.append(best_val_preds)
+    all_val_probs_all.append(best_val_probs)
 
-    test_loader = torch.utils.data.DataLoader(
-        test_dataset,
-        batch_size=BATCH_SIZE,
-        shuffle=False,
-        num_workers=4
-    )
-
-    net.load_state_dict(best_model_state)
-    net.eval()
-
-    test_correct = 0.0
-    test_total = 0.0
-    test_results = []
-
-  
-    all_labels = []
-    all_preds = []
-    all_probs = []
-
-    with torch.no_grad():
-        for data in test_loader:
-            images, labels = data
-            if isinstance(images, list):
-                images = images[0]  
-            images, labels = images.to(device), labels.to(device)
-            outputs, _ = net(images)
-            _, predicted = torch.max(outputs.data, 1)
-            scores = torch.softmax(outputs, dim=1)
-
-            test_total += float(labels.size(0))
-            test_correct += float((predicted == labels).sum())
-
-            
-            all_labels.extend(labels.cpu().numpy())
-            all_preds.extend(predicted.cpu().numpy())
-            all_probs.extend(scores.cpu().numpy())
-
-            for i in range(len(labels)):
-                true_label = labels[i].item()
-                predicted_label = predicted[i].item()
-                score = scores[i].cpu().numpy()
-                test_results.append(f"Fold: {fold_idx + 1}, True: {true_label}, "
-                                    f"Predicted: {predicted_label}, Score: {score}")
-
-    test_acc = 100 * test_correct / test_total
-    print('Fold %d Test Set Accuracy: %.4f%%' % (fold_idx + 1, test_acc))
-
-    return test_acc, np.array(all_labels), np.array(all_preds), np.array(all_probs), test_results
-
+    return best_acc
 
 if __name__ == "__main__":
-   
     if args.results_dir is None:
         results_dir = f"results_{args.model}"
     else:
@@ -275,48 +203,27 @@ if __name__ == "__main__":
     if not os.path.exists(results_dir):
         os.makedirs(results_dir)
 
-   
     indices = list(range(len(train_dataset)))
-
- 
-    all_fold_labels = []
-    all_fold_preds = []
-    all_fold_probs = []
-
     for fold_idx, (train_idx, val_idx) in enumerate(kfold.split(indices)):
-        test_acc, fold_labels, fold_preds, fold_probs, test_results = train_fold(fold_idx, train_idx, val_idx)
-        fold_results.append(test_acc)
-        all_fold_labels.append(fold_labels)
-        all_fold_preds.append(fold_preds)
-        all_fold_probs.append(fold_probs)
+        val_acc = train_fold(fold_idx, train_idx, val_idx)
+        fold_results.append(val_acc)
 
-       
-        with open(os.path.join(results_dir, f"test_results_fold_{fold_idx + 1}.txt"), "w") as file:
-            for result in test_results:
-                file.write(result + "\n")
-
-       
-        np.save(os.path.join(results_dir, f"fold_{fold_idx + 1}_labels.npy"), fold_labels)
-        np.save(os.path.join(results_dir, f"fold_{fold_idx + 1}_preds.npy"), fold_preds)
-        np.save(os.path.join(results_dir, f"fold_{fold_idx + 1}_probs.npy"), fold_probs)
-
-   
     mean_acc = np.mean(fold_results)
     std_acc = np.std(fold_results)
     print("\nK-Fold Cross Validation Results:")
-    print(f"Mean Accuracy: {mean_acc:.4f}% ± {std_acc:.4f}%")
+    print(f"Mean Validation Accuracy: {mean_acc:.4f}% ± {std_acc:.4f}%")
 
-    with open(os.path.join(results_dir, "kfold_results.txt"), "w") as file:
+    with open(os.path.join(results_dir, "kfold_val_results.txt"), "w") as file:
         file.write(f"Model: {args.model}\n")
         file.write(f"Number of folds: {args.n_splits}\n")
-        file.write(f"Mean Accuracy: {mean_acc:.4f}% ± {std_acc:.4f}%\n")
+        file.write(f"Mean Validation Accuracy: {mean_acc:.4f}% ± {std_acc:.4f}%\n")
         file.write("\nIndividual fold results:\n")
         for i, acc in enumerate(fold_results):
             file.write(f"Fold {i + 1}: {acc:.4f}%\n")
 
-   
-    np.save(os.path.join(results_dir, "all_fold_labels.npy"), np.concatenate(all_fold_labels))
-    np.save(os.path.join(results_dir, "all_fold_preds.npy"), np.concatenate(all_fold_preds))
-    np.save(os.path.join(results_dir, "all_fold_probs.npy"), np.concatenate(all_fold_probs))
+    # 保存所有折合并的验证集结果
+    np.save(os.path.join(results_dir, "all_val_labels.npy"), np.concatenate(all_val_labels_all))
+    np.save(os.path.join(results_dir, "all_val_preds.npy"), np.concatenate(all_val_preds_all))
+    np.save(os.path.join(results_dir, "all_val_probs.npy"), np.concatenate(all_val_probs_all))
 
-    print(f"\nAll results have been saved to the directory: {results_dir}") 
+    print(f"\nValidation results saved to: {results_dir}")
